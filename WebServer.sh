7946966943e21b9e440f43fb7d5e70038ef929c4
Jasper Holton <jasper@holton.io>
2015-07-28 11:54:53 -0800

    Fixed server start script

--- WebServer.sh
+++ WebServer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Run web server
 
-cd /Users/jasperholton/Programming/Phaser\ Games/Crimson\ Deep
+cd /Users/jasperholton/Programming/Phaser\ Games/Crimson-Deep
 python -m SimpleHTTPServer
 
